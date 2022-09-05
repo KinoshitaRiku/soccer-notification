3eec88dfd736a9dcc876fdb1c0595e062f0a16ae
KinoshitaRiku <80682674+KinoshitaRiku@users.noreply.github.com>
2022-09-05 21:50:55 +0900

    Update eventBridge.tf

--- eventBridge.tf
+++ eventBridge.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "soccer" {
   name                = "soccer-notification"
   description         = "マンシーの通知を18時に行う"
-  schedule_expression = "cron(0 18 * * ? *)"
+  schedule_expression = "cron(0 9 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "soccer" {
