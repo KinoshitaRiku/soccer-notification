d59e68105e9dac5f6c23dcf2c0bfa7fbbe6ddc6e
KinoshitaRiku <80682674+KinoshitaRiku@users.noreply.github.com>
2023-01-01 00:27:54 +0900

    update *

---
+++ lambda/README.md
@@ -0,0 +1,18 @@
+# lambda
+localでのpythonコードの開発は、venvで構築する。
+```
+# 仮想環境の構築
+python3 -m venv venv
+
+# 仮想環境の有効化
+source ./venv/bin/activate
+
+# 仮想環境の無効化
+deactivate
+
+# pipの最新化
+pip install --upgrade pip
+
+# モジュールのインストール
+pip install -r requirement.txt
+```
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,43 +1,51 @@
 import json
-import boto3  
+import boto3
 from urllib import request
 from datetime import datetime, timedelta
+
+# env
+prod = True
+token = 'a65b95423fb44b0ba757f2098e1d2beb'
+url = 'https://api.football-data.org/v4/teams/65/matches?status=SCHEDULED'
+next_function = 'send-messeage-slack-api-kinoshita-mailCannel'
 
 lambdaClient = boto3.client('lambda')
 
 def lambda_handler(event, context):
   football_data = access_api_football_data()
-  output_text = create_output_text(football_data)
-  action_other_lambda(output_text)
+  date_now = datetime.now()
+  # date_now = datetime(2023, 1, 5, 18, 00, 00, 0) # debag用
+  output_text = create_output_text(football_data, date_now)
+  if prod == True:
+    action_other_lambda(output_text)
+  else:
+    print(output_text)
 
 def access_api_football_data():
-  token = 'a65b95423fb44b0ba757f2098e1d2beb'
-  url = 'https://api.football-data.org/v4/teams/65/matches?status=SCHEDULED'
   headers = {'Accept': 'application/json', 'X-Auth-Token': token}
   request_get = request.Request(url, headers=headers)
   with request.urlopen(request_get) as res:
     body = json.load(res)
   return body
 
-def create_output_text(fotball_data):
-  date_now = datetime.now()
-  # ↓テスト用
-  # date_test = datetime(2022, 10, 11, 18, 00, 00, 0)
-  # date_now = date_test
+def create_output_text(fotball_data, date_now):
   range_date = date_now + timedelta(hours=12)
-  
   matches = fotball_data['matches']
+  # print(get_japan_match_date(matches[0]))# debag用
+  notification_content = ""
+
   for match in matches:
     japan_match_date = get_japan_match_date(match)
-    notification_content = ""
     if date_now < japan_match_date and japan_match_date < range_date:
       content_date = "date: " + japan_match_date.strftime('%Y-%m-%d %H:%M:%S')
       content_matchday = "matchweek: " + str(match['matchday']) # PLの公式はmatchdayではなく、matchweekと表現している。
       content_teams = match['homeTeam']['name'] + " vs " + match['awayTeam']['name']
       notification_content = content_date + '\n' + content_matchday + '\n' + content_teams
+      break
     else:
       notification_content = "なし"
-  return(notification_content)
+
+  return notification_content
 
 def get_japan_match_date(match):
   utc_date = match['utcDate'] # 2022-08-27T14:00:00Z
@@ -51,7 +59,10 @@ def action_other_lambda(data):
   }
   
   lambdaClient.invoke(
-    FunctionName='send-messeage-slack-api-kinoshita-mailCannel',
+    FunctionName=next_function,
     InvocationType='RequestResponse',
     Payload=json.dumps(payload)
   )
+
+if prod == False:
+  lambda_handler("", "")
